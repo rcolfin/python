ebadd7ff4906410960dc0476b081f202e1e3ebf5
Robert Colfin <rcolfin@hotmail.com>
2024-12-03 13:34:26 -0500

    cleaning up code (#6)

--- docker-health-check/docker_health_check/commands/__init__.py
+++ docker-health-check/docker_health_check/commands/__init__.py
@@ -1,4 +1,4 @@
+from docker_health_check.commands import healthcheck
 from docker_health_check.commands.common import cli
-from docker_health_check.commands.healthcheck import health_check
 
-__all__ = ["cli", "health_check"]
+__all__ = ["cli", "healthcheck"]
