c80a8e0e796abce032d8cf61d12d57c6b44fe3f9
Robert Colfin <rcolfin@hotmail.com>
2025-01-10 11:59:09 -0500

    Upgrading ruff

--- dask-test/dask_test/__main__.py
+++ dask-test/dask_test/__main__.py
@@ -2,17 +2,17 @@ import logging
 
 import click
 
-from dask_test import factory, tasks, types
+from dask_test import factory, models, tasks
 
 
 @click.command
 @click.option(
     "-c",
     "--client-type",
-    type=click.Choice([x.name for x in types.ClientType], case_sensitive=False),
+    type=click.Choice([x.name for x in models.ClientType], case_sensitive=False),
 )
 def test(client_type: str) -> None:
-    client = factory.create_client(types.ClientType[client_type])
+    client = factory.create_client(models.ClientType[client_type])
     logging.info("client type is %s", client)
     messages = [f"This is message {x}" for x in range(10)]
     futures = client.map(tasks.echo, messages)
