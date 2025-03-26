047a39a8414243244289938aa9dc32bfae471896
Robert Colfin <robert.m.colfin@gmail.com>
2025-03-26 08:47:07 -0400

    More uv related upgrade changes.

--- docker-health-check/docker_health_check/settings/base.py
+++ docker-health-check/docker_health_check/settings/base.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from docker_health_check.settings.email import EmailSettings
 
 
 class Settings(BaseSettings):
-    email: EmailSettings | None = None
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
 
-    class Config:
-        env_nested_delimiter = "__"
+    email: EmailSettings | None = None
 
 
 settings = Settings()
