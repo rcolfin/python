cbd116dc680cc93a9f4b46a96aba722a5f5230ff
Robert Colfin <robert.m.colfin@gmail.com>
2025-08-26 16:42:41 -0400

    Updating docker images to include label with build date + git commit revision. Updating dependencies across all packages. Replacing aiohttp with curl-cffi.

--- github/github/__init__.py
+++ github/github/__init__.py
@@ -1,15 +1,7 @@
 from __future__ import annotations
 
-import importlib.metadata
-
 from github import commands
+from github.__version__ import __version__
 from github.client import GitHub
-
-# set the version number within the package using importlib
-try:
-    __version__: str | None = importlib.metadata.version("pygithub")
-except importlib.metadata.PackageNotFoundError:
-    # package is not installed
-    __version__ = None
 
 __all__ = ["GitHub", "__version__", "commands", "providers"]
