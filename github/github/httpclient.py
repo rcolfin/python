2cb558257d24a1aec00487f910a1616a25785e2f
Robert Colfin <robert.m.colfin@gmail.com>
2025-08-26 21:24:54 -0400

    Updating docker images to include label with build date + git commit revision. (#13)
    Updating dependencies across all packages.
    Replacing aiohttp with curl-cffi.

--- github/github/httpclient.py
+++ github/github/httpclient.py
@@ -6,10 +6,12 @@ from contextlib import asynccontextmanager
 from http import HTTPMethod, HTTPStatus
 from typing import TYPE_CHECKING, Any, Final, Self, cast
 
-import aiohttp
 import backoff
+from curl_cffi import requests
 
 from github import constants
+
+from . import __version__
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator, AsyncIterable
@@ -21,7 +23,7 @@ PAGINATION_LINK: Final[re.Pattern] = re.compile(r"<(?P<url>[^>]+)>;\s?rel=\"(?P<
 
 class HttpClient:
     def __init__(self) -> None:
-        self._session: aiohttp.ClientSession | None = None
+        self._session: requests.AsyncSession | None = None
 
     async def __aenter__(self) -> Self:
         return self
@@ -47,11 +49,11 @@ class HttpClient:
         next_url: str | None = url
         while next_url:
             async with self._make_request(method, next_url, headers, params=params) as response:
-                yield cast("dict[str, Any]", await response.json())
+                yield cast("dict[str, Any]", response.json())
                 next_url = self._get_next_pagination_link(response)
 
     @staticmethod
-    def _get_next_pagination_link(response: aiohttp.ClientResponse) -> str | None:
+    def _get_next_pagination_link(response: requests.models.Response) -> str | None:
         link = response.headers.get("Link")
         if link is None:
             return None
@@ -64,25 +66,33 @@ class HttpClient:
         self, method: HTTPMethod, url: str, headers: dict[str, str], params: dict[str, Any] | None = None
     ) -> None:
         async with self._make_request(method, url, headers, params=params) as response:
-            assert response.status == HTTPStatus.NO_CONTENT
+            assert response.status_code == HTTPStatus.NO_CONTENT
 
     @backoff.on_exception(
-        backoff.expo, aiohttp.ClientError, max_tries=constants.MAX_RETRIES, max_time=constants.MAX_RETRY_TIME
+        backoff.expo,
+        requests.exceptions.RequestException,
+        max_tries=constants.MAX_RETRIES,
+        max_time=constants.MAX_RETRY_TIME,
     )
     @asynccontextmanager
     async def _make_request(
         self, method: HTTPMethod, url: str, headers: dict[str, str], params: dict[str, Any] | None = None
-    ) -> AsyncGenerator[aiohttp.ClientResponse, None]:
-        session = self._ensure_session()
-        async with session.request(method, url, headers=headers, params=params, raise_for_status=True) as response:
-            yield response
+    ) -> AsyncGenerator[requests.models.Response, None]:
+        async with self._create_session() as session:
+            response = await session.request(str(method), url, headers=headers, params=params)
+            response.raise_for_status()
+        yield response
 
-    def _ensure_session(self) -> aiohttp.ClientSession:
+    def _ensure_session(self) -> requests.AsyncSession:
         if self._session is None:
             self._session = self._create_session()
         return self._session
 
-    def _create_session(self) -> aiohttp.ClientSession:
-        return aiohttp.ClientSession(
-            headers={"Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28"}
+    def _create_session(self) -> requests.AsyncSession:
+        return requests.AsyncSession(
+            headers={
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+                "User-Agent": f"pygithub v{__version__}",
+            }
         )
